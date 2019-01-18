c834f6925ea731118471a8e0710d6746e52f431e
Desi Mandasari <desimandasari1993@gmail.com>
2019-01-18 19:53:37 +0700

    tidy up code

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "manda1007",
-    database: 'todo_app'
+  host: "localhost",
+  user: "root",
+  password: "manda1007",
+  database: "todo_app"
 });
 
-connection.query(
-    'SELECT * FROM `todos`',
-    (error, results) => {
-        if (error) {
-            console.log(error)
-        }
+connection.query("SELECT * FROM `todos`", (error, results) => {
+  if (error) {
+    console.log(error);
+  }
 
-        console.log(results);
-    }
-);
-
+  console.log(results);
+});
